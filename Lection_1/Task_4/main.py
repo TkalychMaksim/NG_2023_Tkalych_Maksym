04b532d32f519dcdb8eb7655c868011059640904
TkalychMaksim <tkalychmaksim@gmail.com>
2023-03-29 01:03:02 +0300

    task 4 adjustment

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -7,31 +7,15 @@ number_c = int(input("Please, enter the c argument: "))
 
 discriminant = number_b * number_b - 4*number_a*number_c
 
+if discriminant < 0:
+    print("The equation has no roots")
+if discriminant == 0:
+    print(f"Result: x = {(-number_b+math.sqrt(discriminant))/(2*number_a)}")
+if discriminant > 0:
+    first_root = (-number_b+math.sqrt(discriminant))/(2*number_a)
+    second_root = (-number_b-math.sqrt(discriminant))/(2*number_a)
+    print(f"Result: x1 = {first_root}, x2 = {second_root}")
 
-def solution():
-    if discriminant < 0:
-        print("The equation has no roots")
-    if discriminant == 0:
-        print(f"Result: x = {(-number_b+math.sqrt(discriminant))/(2*number_a)}")
-    if discriminant > 0:
-        first_root = (-number_b+math.sqrt(discriminant))/(2*number_a)
-        second_root = (-number_b-math.sqrt(discriminant))/(2*number_a)
-        print(f"Result: x1 = {first_root}, x2 = {second_root}")
-
-
-if number_b == 0 | number_c == 0:
-    print("Result: x=0")
-elif number_b == 0:
-    if number_c/number_a >= 0:
-        x1 = math.sqrt(number_c/number_a)
-        x2 = -math.sqrt(number_c/number_a)
-        print(f"Result: x1 = {x1}, x2 = {x2}")
-    else:
-        print("The equation has no roots")
-elif number_c == 0:
-    print(f"Result: x1 = 0, x2 = {-number_b/number_a}")
-else:
-    solution()
 
 
 
