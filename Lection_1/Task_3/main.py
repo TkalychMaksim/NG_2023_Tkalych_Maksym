5e9688aeb7bcbe0773cc2341145194544bf08e29
TkalychMaksim <tkalychmaksim@gmail.com>
2023-03-29 00:57:45 +0300

    task 3 adjustment

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -3,16 +3,19 @@ number_a = float(input("Please, enter the first number: "))
 number_b = float(input("Please, enter the second number: "))
 print("Operations list:\n plus(+) \n minus(-) \n multiply(*) \n squaring(^)")
 operation = input("Choose operation from list: ")
+
 match operation:
     case "plus" | "+":
-        print(f"Result: {number_a + number_b}")
+        result = number_a + number_b
     case "minus" | "-":
-        print(f"Result: {number_a - number_b}")
+        result = number_a - number_b
     case "multiply" | "*":
-        print(f"Result: {number_a * number_b}")
+        result = number_a * number_b
     case "divide" | "/":
-        print(f"Result: {number_a / number_b}")
+        result = number_a / number_b
     case "squaring" | "^":
-        print(f"Result: {number_a}^2 = {number_a * number_a} \n {number_b}^2 = {number_b * number_b}")
+        result = number_a * number_a, number_b * number_b
+
+print(f"Result: {result}")
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
