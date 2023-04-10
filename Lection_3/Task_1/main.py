f6f41db5ad86a5686a071c352df12ee826956355
TkalychMaksim <tkalychmaksim@gmail.com>
2023-04-11 00:46:46 +0300

    Add task 1 files

---
+++ Lection_3/Task_1/main.py
@@ -0,0 +1,41 @@
+def plus(first_num, second_num):
+    return first_num + second_num
+
+
+def minus(first_num, second_num):
+    return first_num - second_num
+
+
+def multiply(first_num, second_num):
+    return first_num * second_num
+
+
+def divide(first_num, second_num):
+    return first_num / second_num
+
+
+def squaring(first_num, second_num):
+    return first_num * first_num, second_num * second_num
+
+
+def main():
+    print("Enter 2 numbers")
+    first_num = float(input("First number: "))
+    second_num = float(input("Second number: "))
+    operation = input(
+        "Choose the operation from list:\n [1] Plus\n [2] Minus\n [3]Multiply\n [4] Divide\n [5] Squaring\n ")
+    match operation:
+        case "1" | "Plus":
+            print(plus(first_num, second_num))
+
+        case "2" | "Minus":
+            print(minus(first_num, second_num))
+        case "3" | "Multiply":
+            print(multiply(first_num, second_num))
+        case "4" | "Divide":
+            print(divide(first_num, second_num))
+        case "5" | "Squaring":
+            print(squaring(first_num, second_num))
+
+
+main()
