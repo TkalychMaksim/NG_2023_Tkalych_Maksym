a8cea60e16d0e2a8ebb5feb06ab88f7ba6c84e1e
TkalychMaksim <tkalychmaksim@gmail.com>
2023-04-30 16:07:40 +0300

    reworked task 1

--- Lection_3/Task_1/main.py
+++ Lection_3/Task_1/main.py
@@ -1,21 +1,15 @@
-def plus(first_num, second_num):
-    return first_num + second_num
-
-
-def minus(first_num, second_num):
-    return first_num - second_num
-
-
-def multiply(first_num, second_num):
-    return first_num * second_num
-
-
-def divide(first_num, second_num):
-    return first_num / second_num
-
-
-def squaring(first_num, second_num):
-    return first_num * first_num, second_num * second_num
+def calculation(operation, first_num, second_num):
+    match operation:
+        case "+":
+            return first_num + second_num
+        case "-":
+            return first_num - second_num
+        case "*":
+            return first_num * second_num
+        case "/":
+            return first_num / second_num
+        case "^":
+            return first_num ** second_num
 
 
 def main():
@@ -23,19 +17,18 @@ def main():
     first_num = float(input("First number: "))
     second_num = float(input("Second number: "))
     operation = input(
-        "Choose the operation from list:\n [1] Plus\n [2] Minus\n [3]Multiply\n [4] Divide\n [5] Squaring\n ")
+        "Choose the operation from list:\n [1] Plus\n [2] Minus\n [3] Multiply\n [4] Divide\n [5] Degree\n ")
     match operation:
         case "1" | "Plus":
-            print(plus(first_num, second_num))
-
+            print(calculation("+", first_num, second_num))
         case "2" | "Minus":
-            print(minus(first_num, second_num))
+            print(calculation("-", first_num, second_num))
         case "3" | "Multiply":
-            print(multiply(first_num, second_num))
+            print(calculation("*", first_num, second_num))
         case "4" | "Divide":
-            print(divide(first_num, second_num))
-        case "5" | "Squaring":
-            print(squaring(first_num, second_num))
+            print(calculation("/", first_num, second_num))
+        case "5" | "Degree":
+            print(calculation("^", first_num, second_num))
 
 
 main()
