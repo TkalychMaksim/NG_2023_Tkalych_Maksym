1328287a71beadb0860296bfb2b609af0a05d204
TkalychMaksim <tkalychmaksim@gmail.com>
2023-06-01 03:40:23 +0300

    updated task 1

--- Lection_3/Task_1/main.py
+++ Lection_3/Task_1/main.py
@@ -1,21 +1,15 @@
-def plus(first_num, second_num):
-    return first_num + second_num
-
-
-def minus(first_num, second_num):
-    return first_num - second_num
-
-
-def multiply(first_num, second_num):
-    return first_num * second_num
-
-
-def divide(first_num, second_num):
-    return first_num / second_num
-
-
-def degree(first_num, second_num):
-    return first_num ** second_num
+def calculation(operation, first_num, second_num):
+    match operation:
+        case "+":
+            return first_num + second_num
+        case "-":
+            return first_num - second_num
+        case "*":
+            return first_num * second_num
+        case "/":
+            return first_num / second_num
+        case "^":
+            return first_num ** second_num
 
 
 def main():
@@ -26,15 +20,15 @@ def main():
         "Choose the operation from list:\n [1] Plus\n [2] Minus\n [3] Multiply\n [4] Divide\n [5] Degree\n ")
     match operation:
         case "1" | "Plus":
-            print(plus(first_num, second_num))
+            print(calculation("+", first_num, second_num))
         case "2" | "Minus":
-            print(minus(first_num, second_num))
+            print(calculation("-", first_num, second_num))
         case "3" | "Multiply":
-            print(multiply(first_num, second_num))
+            print(calculation("*", first_num, second_num))
         case "4" | "Divide":
-            print(divide(first_num, second_num))
+            print(calculation("/", first_num, second_num))
         case "5" | "Degree":
-            print(degree(first_num, second_num))
+            print(calculation("^", first_num, second_num))
 
 
 main()
